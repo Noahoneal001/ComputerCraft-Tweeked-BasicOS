b3af9fdea76b60c0cfbb5d9b60ba52ecb3064019
Noahoneal001 <143848565+Noahoneal001@users.noreply.github.com>
2024-05-21 15:49:10 -0700

    UPDATE

--- BasicOS.lua
+++ BasicOS.lua
@@ -1,5 +1,7 @@
 --First make variables
 local screenX, screenY = term.getSize()
+local isLogined = false
+local logo = paintutils.loadImage("BasicLogo.nfp")
 
 --Second we prevent the user from force closeing the OS program before they past the login screen (We are saving the abilty to terminate to give back later)
 --Josh:"The Lord yeet'th, and The Lord yoink'th away - Abraham Lincon, 2"
@@ -9,9 +11,26 @@ os.pullEvent = os.pullEventRaw
 
 --Third we clear the screen and set the cursor postion.
 term.clear()
-
 term.setCursorPos(1,1)
 
 -- Now we are createing the start screen
-paintutils.drawFilledBox(1, 1, 51, 19, colors.red)
+term.setBackgroundColor(colors.lightGray)
+term.clear()
 
+term.setTextColor(colors.black)
+
+term.setCursorPos(screenX/5,screenY/4)
+paintutils.drawImage(logo,term.getCursorPos())
+
+paintutils.drawLine(screenX/5 + 8, screenY/4 + 7, screenX/5 + 18, screenY/4 + 7 , colors.white)
+term.setCursorPos(screenX/5 + 8, screenY/4 + 7)
+term.write("Username")
+
+paintutils.drawLine(screenX/5 + 8, screenY/4 + 9, screenX/5 + 18, screenY/4 + 9 , colors.white)
+term.setCursorPos(screenX/5 + 8, screenY/4 + 9)
+term.write("Password")
+
+while not isLogined do 
+  read()
+  error()
+end
