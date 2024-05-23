b457b38d1432fca00a0e79aaa3bf845270138565
Noahoneal001 <143848565+Noahoneal001@users.noreply.github.com>
2024-05-23 15:43:46 -0700

    LOGIN DONE

--- BasicOS.lua
+++ BasicOS.lua
@@ -3,6 +3,10 @@ local screenX, screenY = term.getSize()
 local isLogined = false
 local logo = paintutils.loadImage("/BasicOS_Files/BasicLogo.nfp")
 local ogScreen = term.current()
+local usernameInput = "Username"
+local passwordInput = "Password"
+local username = "Nicehoan123"
+local password = "FIREBALL!"
 local BasicOSHelper = require("BasicOS_Helper")
 
 --Second we prevent the user from force closeing the OS program before they past the login screen (We are saving the abilty to terminate to give back later)
@@ -21,19 +25,53 @@ term.clear()
 
 term.setTextColor(colors.black)
 
-BasicOSHelper.drawImageAt(logo, screenX/5, screenY/4)
+BasicOSHelper.drawImageAt(logo, math.floor(screenX/5), math.floor(screenY/4))
 
-local usernameInput = window.create(term.current(), screenX/5 + 8, screenY/4 + 7, 12, 1)
-local passwordInput = window.create(term.current(), screenX/5 + 8, screenY/4 + 9, 12, 1)
+local usernameInputZone = window.create(term.current(), math.floor(screenX/5 + 8), math.floor(screenY/4 + 7), 12, 1)
+local passwordInputZone = window.create(term.current(), math.floor(screenX/5 + 8), math.floor(screenY/4 + 9), 12, 1)
 
-BasicOSHelper.makeLoginInput(usernameInput, colors.white, colors.black, "Username")
-BasicOSHelper.makeLoginInput(passwordInput, colors.white, colors.black, "Password")
+BasicOSHelper.makeLoginInput(usernameInputZone, colors.white, colors.black, "Username")
+BasicOSHelper.makeLoginInput(passwordInputZone, colors.white, colors.black, "Password")
+
+term.setBackgroundColor(colors.green)
+term.setCursorPos(math.floor(screenX/5 + 11), math.floor(screenY/4 + 11))
+write("LOGIN")
 
 while not isLogined do
   local event, button, x, y = os.pullEvent("mouse_click")
 
   if event == "mouse_click" then
-    if x >= screenX/5 + 8 and x <= screenX/5 + 8 + 12 and y == screenY/4 + 7 then
-      term.redirect(usernameInput)
+    if x >= math.floor(screenX/5 + 8) and x <= math.floor(screenX/5 + 8 + 12) and y == math.floor(screenY/4 + 7) then
+      -- if BasicOS_Helper.isAreaClickedLine(x, y, math.floor(screenX/5 + 8))
+      term.redirect(usernameInputZone)
+      term.clear()
+      term.setCursorPos(1,1)
+      usernameInput = read()
+      write(usernameInput)
+      term.redirect(ogScreen)
     end
+    if x >= math.floor(screenX/5 + 8) and x <= math.floor(screenX/5 + 8 + 12) and y == math.floor(screenY/4 + 9) then
+      term.redirect(passwordInputZone)
+      term.clear()
+      term.setCursorPos(1,1)
+      passwordInput = read("*")
+      write(string.rep("*", string.len(passwordInput)))
+      term.redirect(ogScreen)
+    end
+    if x >= math.floor(screenX/5 + 11) and x <= math.floor(screenX/5 + 16) and y == math.floor(screenY/4 + 11) then
+      if usernameInput == username and passwordInput == password then
+        os.pullEvent = yoink
+        term.setBackgroundColor(colors.black)
+        term.clear()
+        term.setCursorPos(1,1)
+        error()
+      else
+        term.setCursorPos(math.floor(screenX/5 + 9), math.floor(screenY/4 + 13))
+        term.setBackgroundColor(colors.lightGray)
+        term.setTextColor(colors.red)
+        write("INCORRECT")
+        term.setTextColor(colors.black)
+      end
+    end
+  end
 end
--- BasicOS_Helper.lua
+++ BasicOS_Helper.lua
@@ -1,15 +1,18 @@
 local function makeLoginInput(window, boxColor, TextColor, text)
-    window.setBackgroundColor(boxColor)
-    window.clear()
-    window.setTextColor(TextColor)
-    window.setCursorPos(1,1)
-    window.write(text)
-  end
-  
-  local function drawImageAt(image, x, y)
-    term.setCursorPos(x,y)
-    paintutils.drawImage(image,term.getCursorPos())
-  end
-  
-  return({ makeLoginInput = makeLoginInput, drawImageAt = drawImageAt })
-  
+  window.setBackgroundColor(boxColor)
+  window.clear()
+  window.setTextColor(TextColor)
+  window.setCursorPos(1,1)
+  window.write(text)
+end
+
+local function drawImageAt(image, x, y)
+  term.setCursorPos(x,y)
+  paintutils.drawImage(image,term.getCursorPos())
+end
+
+local function isAreaClickedLine(clickX, clickY, minX, maxX, y)
+  return clickX >= minX and clickX <= maxX and clickY == y
+end
+
+return({ makeLoginInput = makeLoginInput, drawImageAt = drawImageAt, isAreaClickedLine = isAreaClickedLine })
